5b3ab22e1cb47409bc6774e85ec4df78ce70103a
Em Weisiger <emweisiger@gmail.com>
2016-02-05 10:38:31 -0700

    Update and rename Arrays_Arrays-DS to Arrays_Arrays-DS.py

--- Arrays_Arrays-DS.py
+++ Arrays_Arrays-DS.py
@@ -1,13 +1,14 @@
 # HackerRank_DataStructures > Arrays > Arrays-DS
 
+from __future__ import print_function
 import sys
 
 n = int(raw_input().strip())
 arr = map(int,raw_input().strip().split(' '))
 
 for i in reversed(arr):
-    print i
+    print(i, end = " ")
     
     # for item in my_list[::-1]:
     #   print item
-    # This is slightly slower than using reversed in Python 2.7
+    # This is slightly slower than using reversed in Python 2.7.
